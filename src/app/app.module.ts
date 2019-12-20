55be32c6789a949bac4bdbd727b34ec1db320e5c
faisaldeveloper <faisal.shahzad786@gmail.com>
2019-12-20 22:35:40 +0500

    Fixed bug in Material design setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//import { AppRoutingModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 import { RouterModule} from '@angular/router';
 
@@ -23,6 +25,7 @@ import { MatToolbarModule,
   ],
   imports: [
     BrowserModule,
+    //AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
     MatToolbarModule,
