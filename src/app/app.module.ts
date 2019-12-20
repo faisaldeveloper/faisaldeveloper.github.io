3317b4162b13ac38fb11755040c24cd049908047
faisaldeveloper <faisal.shahzad786@gmail.com>
2019-12-20 16:48:55 +0500

    configured basic angular app and added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+
+    RouterModule.forRoot([
+      {
+         path: 'abt-cmp',
+         component: AboutComponent
+      }
+   ])
   ],
   providers: [],
   bootstrap: [AppComponent]
